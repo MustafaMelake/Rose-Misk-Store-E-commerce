3a92805a41e0f8226db117ffb47586b8a017811a
Mustafa melake <mustafamelake@gmail.com>
2026-02-02 01:50:28 +0200

    Path fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/rose-misk-store",
+  base: "/Rose-Misk-Store-E-commerce/",
 });
